212ee287faff2826aa554fc075a34d0bbcd28aae
Samrudhi Shikerkar <mscids2024.samrudhi@unigoa.ac.in>
2025-10-30 22:48:40 +0530

    Defined TransactionManager class structure

---
+++ transactions.py
@@ -0,0 +1,6 @@
+# transactions.py - transaction management module"
+# Author: Samrudhi Shikerkar
+# Handles adding, viewing, and categorizing income and expense transactions.
+
+class TransactionManager:
+    pass
