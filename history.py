d0bec2d12a03a9c88636c7869355ddf323dff371
Keziah <mscids2024.keziah@unigoa.ac.in>
2025-10-31 13:27:21 +0530

    Add filter_by_date() to filter transactions within a date range

--- history.py
+++ history.py
@@ -1,3 +1,7 @@
 #History.py
 #Author: Keziah Blossom Pereira
 #Provides methods for transaction history display and filtering.
+
+class HistoryManager:
+    def __init__(self, transaction_manager):
+        self.tm = transaction_manager
