c88caf2f1960b14a8b8669924eaa78e7364799e9
Falak-star18 <mscids2024.falak@unigoa.ac.in>
2025-10-31 16:37:50 +0530

    Implemented get_budget() to return allocated budget with default value 0

--- budgeting.py
+++ budgeting.py
@@ -9,4 +9,6 @@ class BudgetManager:
         self.budgets[category] = amount
 
     def get_budget(self, category):
-        
+        """Retrieve the allocated budget for a category."""
+        return self.budgets.get(category, 0)
+
