d3617347460bfbf159657e98b6641a671f30611a
Anusha <mscids2024.anusha@unigoa.ac.in>
2025-10-31 13:37:20 +0530

    update: added labels

--- visualization.py
+++ visualization.py
@@ -12,3 +12,14 @@ class VisualizationManager:
     def plot_expense_pie(self):
         """Show pie chart of expense distribution across categories."""
         category_totals = {}
+
+        for t in self.transactions:
+            if t['type'] == 'expense':
+                category_totals[t['category']] = category_totals.get(t['category'], 0) + t['amount']
+        labels = list(category_totals.keys())
+        sizes = list(category_totals.values())
+        plt.pie(sizes, labels=labels, autopct='%1.1f%%')
+        plt.title('Expense by Category')
+        plt.show()
+
+        
